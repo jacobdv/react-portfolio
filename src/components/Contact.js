2a0584f4d93d05387129584d98ed5c6c2779b3b8
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-25 13:30:23 -0700

    Formatting. Not sure why email link isn't working.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,7 +34,7 @@ export default function Contact() {
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3040456467647!2d-122.62750258443985!3d45.54420887910194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a72aad4f4549%3A0xbed4dbb063837473!2s2933%20NE%2037th%20Ave%2C%20Portland%2C%20OR%2097212!5e0!3m2!1sen!2sus!4v1629916330564!5m2!1sen!2sus"
                 />
                 <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-                    <div className="lg:w-1/2 px-6">
+                    <div className="lg:w-5/12 px-6">
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                             ADDRESS
                         </h2>
@@ -43,7 +43,7 @@ export default function Contact() {
                             Portland, OR 97212
                         </p>
                     </div>
-                    <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                    <div className="lg:w-7/12 px-6 mt-4 lg:mt-0">
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                             EMAIL
                         </h2>
