a456fbb7a6311da4d01fcc5d9c6bd24a59f16ecf
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-25 11:34:56 -0700

    Updating information.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,7 +31,7 @@ export default function Contact() {
                     marginHeight={0}
                     marginWidth={0}
                     style={{ filter: "opacity(0.7)" }}
-                    src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.3040456467647!2d-122.62750258443985!3d45.54420887910194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5495a72aad4f4549%3A0xbed4dbb063837473!2s2933%20NE%2037th%20Ave%2C%20Portland%2C%20OR%2097212!5e0!3m2!1sen!2sus!4v1629916330564!5m2!1sen!2sus"
                 />
                 <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
                     <div className="lg:w-1/2 px-6">
@@ -39,8 +39,8 @@ export default function Contact() {
                             ADDRESS
                         </h2>
                         <p className="mt-1">
-                            97 Warren St. <br />
-                            New York, NY 10007
+                            2933 NE 37th Ave <br />
+                            Portland, OR 97212
                         </p>
                     </div>
                     <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
@@ -48,12 +48,12 @@ export default function Contact() {
                             EMAIL
                         </h2>
                         <a className="text-indigo-400 leading-relaxed">
-                            reedbarger@email.com
+                            mrjacobdevries@gmail.com
                         </a>
                         <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                             PHONE
                         </h2>
-                        <p className="leading-relaxed">123-456-7890</p>
+                        <p className="leading-relaxed">(541) 539-1828</p>
                     </div>
                 </div>
                 </div>
