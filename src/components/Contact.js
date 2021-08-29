b4c28f93a1ed661f49a56ebdceffa11f49cd943c
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-29 11:05:17 -0700

    Testing button functionality.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -103,6 +103,7 @@ export default function Contact() {
                             />
                         </div>
                         <button
+                            onclick='handleSubmit(e);'
                             type="submit"
                             className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
                             Submit
