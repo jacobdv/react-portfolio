01bedd225bfd014e4cc242ca1d69c4afd15b468c
Jacob de Vries <mrjacobdevries@gmail.com>
2021-08-31 12:03:10 -0700

    Undoing.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function Navbar() {
     return (
         <header className="bg-gray-800 md:sticky top-0 z-99">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center fixed">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <a className="title-font font-medium text-white mb-4 md:mb-0">
                     <a href="#about" className="ml-3 text-xl">
                         Jacob de Vries
